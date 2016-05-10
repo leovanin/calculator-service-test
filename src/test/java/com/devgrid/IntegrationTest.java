fc37f4ea784e91c32ac52519f444765fbde60b6c
gonka <goncalo.moraes@gmail.com>
2016-05-10 15:19:30 -0300

    Initial commit with basic tests for health check and multiply function.

---
+++ src/test/java/com/devgrid/IntegrationTest.java
@@ -0,0 +1,33 @@
+package com.devgrid;
+
+import com.jayway.restassured.response.Response;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+
+public class IntegrationTest {
+
+    @Test
+    public void testHealthCheck() {
+        Response response = get("http://localhost:8200/api/health/alive").andReturn();
+        assertEquals(response.getStatusCode(), 200);
+    }
+
+
+    @Test
+    public void testMultiplyCase1() {
+        Response response = get("http://localhost:8200/calculator/multiply?a=3&b=4").andReturn();
+        assertEquals(response.getStatusCode(), 200);
+        assertEquals(response.body().asString(), "12");
+    }
+
+
+    @Test
+    public void testMultiplyCase2() {
+        Response response = get("http://localhost:8200/calculator/multiply?a=5&b=7").andReturn();
+        assertEquals(response.getStatusCode(), 200);
+        assertEquals(response.body().asString(), "35");
+    }
+
+}
